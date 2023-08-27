f95f077a80259d233cf0db03bc3779ce5df6c3a3
nbarnabee <nicole.barnabee@gmail.com>
2023-08-27 15:07:49 +0200

    Take axios instance into use

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,34 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted, watchEffect } from "vue";
+import TriviaService from "../services/TriviaService";
+import QuestionBox from "../components/QuestionBox.vue";
+const token = ref("");
+
+onMounted(() => {
+  watchEffect(async () => {
+    try {
+      const response = await TriviaService.getToken();
+      token.value = response.data.token;
+    } catch (error) {
+      console.log(error);
+    }
+  });
+});
+</script>
 
 <template>
   <main>
     <h1>Welcome to Quiz Master!</h1>
+    <QuestionBox :token="token" />
   </main>
 </template>
 
 <style scoped>
 main {
-  display: grid;
-  place-content: center;
-  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  margin-top: 8rem;
+  gap: 4rem;
+  align-items: center;
 }
 </style>
