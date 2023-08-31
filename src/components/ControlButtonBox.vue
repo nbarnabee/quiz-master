652a5670094dc63c2a1841cb713fd652e446a2b4
nbarnabee <nicole.barnabee@gmail.com>
2023-08-31 17:27:24 +0200

    Remove extraneous code

--- src/components/ControlButtonBox.vue
+++ src/components/ControlButtonBox.vue
@@ -1,24 +1,17 @@
 <script setup>
 import { useQuestionStore } from "../stores/question";
-import { storeToRefs } from "pinia";
 
 const questionStore = useQuestionStore();
-const { questionNumber, questionAnswered } = storeToRefs(questionStore);
-
-function answerQuestion() {
-  questionAnswered.value = true;
-  questionStore.checkAnswer();
-}
 </script>
 
 <template>
   <span>
     <button
-      v-if="questionAnswered"
+      v-if="questionStore.questionAnswered"
       type="button"
       @click="
         [
-          questionNumber < questionStore.questions.length - 1
+          questionStore.questionNumber < questionStore.questions.length - 1
             ? questionStore.advanceQuestionNum()
             : questionStore.getQuestions()
         ]
@@ -26,7 +19,7 @@ function answerQuestion() {
     >
       Next Question
     </button>
-    <button v-else type="button" @click="answerQuestion()">Submit</button>
+    <button v-else type="button" @click="questionStore.checkAnswer()">Submit</button>
   </span>
 </template>
 
