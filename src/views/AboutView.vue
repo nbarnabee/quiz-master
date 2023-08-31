aec65e49a6dff294f882db0589b6186eb35d506c
nbarnabee <nicole.barnabee@gmail.com>
2023-08-31 17:33:25 +0200

    Flesh out the about page a tiny bit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>All about Quiz Master</h1>
+    <p>
+      I was playing around with Pinia and thought that rewriting an old trivia training project
+      would make for the perfect test case: if I wanted to write the page in a proper "Vue-like" way
+      I'd need to break it down into components, which would need to share quite a lot of
+      information. What better use-case for a state management framework?
+    </p>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+.about {
+  max-width: 600px;
+  margin-inline: auto;
+  margin-top: 5rem;
 }
 </style>
